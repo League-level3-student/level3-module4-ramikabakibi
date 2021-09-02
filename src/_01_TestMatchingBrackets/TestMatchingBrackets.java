6019c0ce4dc1c91c7a01b23822f657bf94bd5292
student <student@ip-172-31-63-107.us-west-2.compute.internal>
2021-09-01 18:31:33 -0700

    Finished Test Matching Brackets, Intro to Queue, now need to start on baseball tickets, its pretty tricky, lot of factors to take into account so try to think through problem, need to monitor friends place in line, how many tickets they have left to buy, if other people leave the queue after getting all the tickets they need(that reduces the amount of time it takes for friend to get tickets), ect..

--- src/_01_TestMatchingBrackets/TestMatchingBrackets.java
+++ src/_01_TestMatchingBrackets/TestMatchingBrackets.java
@@ -10,12 +10,25 @@ public class TestMatchingBrackets {
 	//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
 	static Stack<Character> chars=new Stack<Character>();
     public static boolean doBracketsMatch(String b) {
-    	for(int i=0; i<b.length()-1; i++) {
+    	for(int i=0; i<b.length(); i++) {
     		if(b.charAt(i)=='{') {
-    			chars.add(b.charAt(i));
+    			chars.push(b.charAt(i));
     			
     		}
+    		else if(!chars.isEmpty()) {
+    			if(b.charAt(i)=='}') {
+    				chars.pop();
+    			}
+    		}
+    		else if(chars.isEmpty()&&b.charAt(i)=='}') {
+    			return false;
+    		}
     	}
+    	if(chars.isEmpty()) {
+    		return true;
+    	}
+    	return false;
+    
     	//BASICALLY THE LOGIC FOR THIS IS THAT WE HAVE STRINGS AND NEED TO SEE IF WE HAVING MATCHING LEFT AND RIGHT
     	//CURLY BRACKETS, SO THE FOR LOOP ABOVE SEES IF THERE IS A LEFT CURLY AND IF THERE IS THEN IT PUSHES IT TO
     	//A STACK. NOW I NEED ANOTHER FOR LOOP GOING THROUGH THE STRING B TO SEE IF THERE IS A RIGHT CURLY, AND IF
@@ -23,6 +36,6 @@ public class TestMatchingBrackets {
     	//THE FOR LOOPS, THE STACK IS EMPTY, THEN EVERYTHING WAS MATCHED,RETURN TRUE AND THEN ALSO, IF THERE IS A RIGHT CURLY 
     	//BRACKET, AND THE STACK IS EMPTY(NOTHING TO POP) THAT MEANS THAT ALL THE BRACKETS WERE NOT MATCHED, RETURN FALSE
     	
-        return false;
+      
     }
 }
--- src/_02_IntroToQueues/_01_IntroToQueue.java
+++ src/_02_IntroToQueues/_01_IntroToQueue.java
@@ -30,26 +30,61 @@ package _02_IntroToQueues;
  * Repeat until there are no more elements in the stack and queue
  */
 
+import java.util.ArrayDeque;
+import java.util.Random;
+import java.util.Stack;
+
 public class _01_IntroToQueue {
-    // 1. Create a Stack of Doubles using the Stack class
+    static // 1. Create a Stack of Doubles using the Stack class
     //    Note: you have to use the capitalized Double and not double
-    
+    Stack<Double> doubles=new Stack<Double>();
     // 2. Use a loop to add 100 random doubles between 0 and 100 to the Stack
-    
-    // 3. Create a Queue of Doubles using the ArrayDeque class
-    //    Note: you have to use the capitalized Double and not double
+    public static void main(String[] args) {
+    	for(int i=0; i<100; i++) {
+        	Random rand=new Random();
+        	double random=rand.nextDouble()*100;
+        	doubles.push(random);
+        	
+        }
+        // 3. Create a Queue of Doubles using the ArrayDeque class
+        //    Note: you have to use the capitalized Double and not double
+        ArrayDeque<Double> queue=new ArrayDeque<Double>();
+        // 4. Pop off 5 elements from the Stack and add them to the Queue 
+        for(int i=0; i<5; i++) {
+        	queue.add(doubles.pop());
+        }
+        // 5. Print and remove a random number of elements, from 1 to 5 elements,
+        //    from the front of the Queue. Example:
+        //    "removing 3 elements from Queue: 25 57 2"
+        while(!doubles.isEmpty()&&!queue.isEmpty()) {
+        	Random rand=new Random();
+        	int num=rand.nextInt(5)+1;
+        	System.out.print("removing "+num+"elements from Queue: ");
+        	for(int i=0; i<num; i++) {
+        		double h=queue.remove();
+        		System.out.print(h+" ");
+        	}
+        	System.out.println();
+        // 6. Pop off as many elements from the stack to fill the Queue with 5
+        //    elements. If there aren't enough elements in the Stack to fill the 
+        //    queue, fill the queue as much as possible. 
+        int remainder=5-queue.size();
+        if(doubles.size()==remainder || doubles.size()>remainder) {
+        for(int i=0; i<remainder; i++) {
+        	queue.add(doubles.pop());
+        }
+        }
+        else {
+        	int size=doubles.size();
+        	for(int i=0; i<size; i++) {
+        		queue.add(doubles.pop());
+        	}
+        }
+        // 7. Loop until there are no more elements in either the Stack or Queue
+        //    and all the elements are printed
+        
+    }
 
-    // 4. Pop off 5 elements from the Stack and add them to the Queue 
-
-    // 5. Print and remove a random number of elements, from 1 to 5 elements,
-    //    from the front of the Queue. Example:
-    //    "removing 3 elements from Queue: 25 57 2"
+    }
+	}
     
-    // 6. Pop off as many elements from the stack to fill the Queue with 5
-    //    elements. If there aren't enough elements in the Stack to fill the 
-    //    queue, fill the queue as much as possible. 
-    
-    // 7. Loop until there are no more elements in either the Stack or Queue
-    //    and all the elements are printed
-    
-}
